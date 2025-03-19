f8bf80a2c6afa58bc666535af9eaa8b7561bc48b
jitae5moon <jitae5moon@gmail.com>
2025-03-20 00:27:07 +0900

    feat: Add JpaSpecificationExecutor for dynamic query

--- web-application/backend/article-service/src/main/java/org/project/articleservice/repository/ArticleRepository.java
+++ web-application/backend/article-service/src/main/java/org/project/articleservice/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package org.project.articleservice.repository;
 
 import org.project.articleservice.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
 }
---
+++ web-application/backend/article-service/src/main/java/org/project/articleservice/repository/specification/ArticleSpecification.java
@@ -0,0 +1,20 @@
+package org.project.articleservice.repository.specification;
+
+import org.project.articleservice.domain.Article;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ArticleSpecification {
+
+    public static Specification<Article> likeTitle(String keyword) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + keyword + "%");
+    }
+
+    public static Specification<Article> likeContent(String keyword) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("content"), "%" + keyword + "%");
+    }
+
+    public static Specification<Article> likeCreatedBy(String keyword) {
+        return ((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("createdBy"), "%" + keyword + "%"));
+    }
+
+}
