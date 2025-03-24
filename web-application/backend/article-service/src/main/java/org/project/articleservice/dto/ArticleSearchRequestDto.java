cc01d1bfd6f2b17de7c6e475aaf4c5c4e433edff
jitae5moon <jitae5moon@gmail.com>
2025-03-25 00:26:05 +0900

    feat: Combine PaginationDto with ArticleSearchRequestDto
    - Consider to separate it later

--- web-application/backend/article-service/src/main/java/org/project/articleservice/dto/ArticleSearchRequestDto.java
+++ web-application/backend/article-service/src/main/java/org/project/articleservice/dto/ArticleSearchRequestDto.java
@@ -2,6 +2,14 @@ package org.project.articleservice.dto;
 
 public record ArticleSearchRequestDto(
         String searchType,
-        String searchWord
+        String searchWord,
+        Integer currentPage,
+        Integer pageSize
 ) {
+
+    public ArticleSearchRequestDto {
+        currentPage = currentPage == null ? 1 : currentPage;
+        pageSize = pageSize == null ? 10 : pageSize;
+    }
+
 }
--- web-application/backend/article-service/src/main/java/org/project/articleservice/dto/PaginationDto.java
+++
@@ -1,8 +0,0 @@
-package org.project.articleservice.dto;
-
-public record PaginationDto(
-        Integer currentPage,
-        Integer pageSize
-) {
-
-}
