bbe2603c6db9d4109267248fb335060315326eab
jitae5moon <jitae5moon@gmail.com>
2025-03-25 01:26:05 +0900

    feat: Add ui for attachments

--- web-application/backend/article-service/src/main/java/org/project/articleservice/dto/ArticleResponseDto.java
+++ web-application/backend/article-service/src/main/java/org/project/articleservice/dto/ArticleResponseDto.java
@@ -3,8 +3,10 @@ package org.project.articleservice.dto;
 import org.project.articleservice.domain.Article;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public record ArticleResponseDto(Long id, String title, String content, String createdBy, LocalDateTime createdDate, String modifiedBy, LocalDateTime modifiedDate) {
+public record ArticleResponseDto(Long id, String title, String content, String createdBy, LocalDateTime createdDate, String modifiedBy, LocalDateTime modifiedDate, List<AttachmentResponseDto> attachments) {
 
     public static ArticleResponseDto from(Article article) {
         return new ArticleResponseDto(
@@ -14,7 +16,8 @@ public record ArticleResponseDto(Long id, String title, String content, String c
                 article.getCreatedBy(),
                 article.getCreatedDate(),
                 article.getModifiedBy(),
-                article.getModifiedDate()
+                article.getModifiedDate(),
+                article.getAttachments().stream().map(AttachmentResponseDto::from).collect(Collectors.toList())
         );
     }
 
