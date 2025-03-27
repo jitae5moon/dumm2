2f815c17b1b0701e71af92eab1b47534b98edebf
jitae5moon <jitae5moon@gmail.com>
2025-03-27 21:09:37 +0900

    feat: Change pagination bar format

--- web-application/backend/article-service/src/main/java/org/project/articleservice/service/PaginationService.java
+++ web-application/backend/article-service/src/main/java/org/project/articleservice/service/PaginationService.java
@@ -12,9 +12,9 @@ public class PaginationService {
     private static final Integer BAR_SIZE = 5;
 
     public List<Integer> getPaginationBarNumbers(int currentPage, int totalPages) {
-        int endPage = (currentPage - BAR_SIZE / 2 >= 1) ? currentPage + BAR_SIZE / 2 : BAR_SIZE;
+        int endPage = (int) (Math.ceil((double) currentPage / BAR_SIZE) * BAR_SIZE);
         if (endPage > totalPages) endPage = totalPages;
-        int startPage = Math.max(1, endPage - BAR_SIZE + 1);
+        int startPage = endPage - BAR_SIZE + 1;
 
         return IntStream.range(startPage, endPage + 1).boxed().collect(Collectors.toList());
     }
